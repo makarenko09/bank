9f4036af16d7aac4255593efc54e9ab494263101
makarenko09 <vinni0010@mail.ru>
2026-02-27 13:56:07 +0300

    build(dev): Connect Postgres to Liqubase.
    in-prefix 'dev' - mean that on future we can migrate from KeyCloak h2 to someDB, and, for example,
    synchronaze their via 'git commit' on body with  in-prefix 'prod'.
    
    LEARNING BY DOING: an active, experiential approach where you read this article:
    https://medium.com/version-1/db-migrations-using-liquibase-8cecf0209118
    https://contribute.liquibase.com/extensions-integrations/directory/integration-docs/springboot/
    https://docs.liquibase.com/pro/user-guide-4-33/changelog-examples
    https://docs.liquibase.com/pro/user-guide-4-33/what-is-a-changeset

--- myApp/src/main/java/com/example/bankcards/wire/database/infrastructure/secondary/DatabaseConfiguration.java
+++ myApp/src/main/java/com/example/bankcards/wire/database/infrastructure/secondary/DatabaseConfiguration.java
@@ -1,10 +1,22 @@
 package com.example.bankcards.wire.database.infrastructure.secondary;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
+@EnableAsync
 @EnableJpaRepositories(basePackages = { "com.example.bankcards" }, enableDefaultTransactions = false)
-class DatabaseConfiguration {}
+class DatabaseConfiguration {
+    @Bean("taskExecutor")
+    public TaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.initialize();
+        return executor;
+    }
+}
