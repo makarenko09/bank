b53f77f08754f6caa6a4563e7916bb0e5e1c39f4
makarenko09 <vinni0010@mail.ru>
2026-02-27 13:50:20 +0300

    refactor(remove): Dell immutable cards -> init Set<Card> on ClientAccount.java

---
+++ myApp/src/main/java/com/example/bankcards/transaction/domain/Cards.java
@@ -0,0 +1,9 @@
+// package com.example.bankcards.transaction.domain;
+
+// import java.util.Set;
+// import com.example.bankcards.transaction.domain.card.Card;
+
+// public record Cards(Set<Card> cards) {
+
+// public static final Cards EMPTY = new Cards(null);
+// }
