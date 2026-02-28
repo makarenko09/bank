33b6ec34d642fcd0423ec1e4c6943e217fd13389
makarenko09 <vinni0010@mail.ru>
2026-02-28 22:53:52 +0300

    refactor(remove): dell

--- myApp/src/main/java/com/example/bankcards/transaction/infrastructure/secondary/AdministratorClientManagment.java
+++ myApp/src/main/java/com/example/bankcards/transaction/infrastructure/secondary/AdministratorClientManagment.java
@@ -1,6 +1,7 @@
 package com.example.bankcards.transaction.infrastructure.secondary;
 
-import org.slf4j.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.example.bankcards.shared.error.domain.Assert;
@@ -22,12 +23,11 @@ public class AdministratorClientManagment {
     private final CardRepository repository;
 
     public ClientAccount createAccount(String ownerName) {
-       logger.bug("ownmer - {}", ownerName);
-       
+        logger.bug("ownmer - {}", ownerName);
+
         ClientAccount clientaccount = ClientAccount.ClientAccountBuilder(ownerName);
         // Assert.field("newCreatedClietnAccount",
         // clientaccount.getOwnerName()).equals(ownerName);
-        
 
         Card userWithNewCard = activatedCard.addUserNewCard(clientaccount);
         repository.save(userWithNewCard);
