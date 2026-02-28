00cee4213fb1aea7a18b49c86d7694682875c2de
makarenko09 <vinni0010@mail.ru>
2026-02-28 09:46:03 +0300

    build(liquibase): migration changelog liquibase to scripts DDL

--- myApp/src/main/resources/config/liquibase/changelog/migration/start_development_db.sql
+++ myApp/src/main/resources/config/liquibase/changelog/migration/start_development_db.sql
@@ -1,1 +1,25 @@
 -- liquibase formatted sql
+
+-- changeset lybov:1
+
+-- public.cards определение
+
+-- Drop table
+
+-- DROP TABLE public.cards;
+
+CREATE TABLE public.cards (
+	id uuid NOT NULL,
+	balance numeric NULL,
+	expiry_end date NOT NULL,
+	"number" varchar(255) NOT NULL,
+	user_id uuid NOT NULL,
+	status varchar(20) NULL,
+	CONSTRAINT cards_pkey PRIMARY KEY (id),
+	CONSTRAINT cards_status_check CHECK (((status)::text = ANY ((ARRAY['ACTIVE'::character varying, 'BLOCKED'::character varying, 'EXPIRED'::character varying])::text[])))
+);
+
+-- Permissions
+
+ALTER TABLE public.cards OWNER TO bank;
+GRANT ALL ON TABLE public.cards TO bank;
